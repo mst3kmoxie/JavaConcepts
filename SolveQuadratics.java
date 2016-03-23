0c3ab2ba9b91406489e605520147e0ccdacd8c39
Dawn Carroll <dsc254@gmail.com>
2016-03-23 13:43:07 +0000

    Update SolveQuadratics.java

--- SolveQuadratics.java
+++ SolveQuadratics.java
@@ -19,17 +19,18 @@ public class SolveQuadratics {
 		QuadraticEquation test = new QuadraticEquation(testA, testB, testC);
 		
 		if (test.hasSolution()) {
-			System.out.println("There is at least one real solution given your input.");
+			System.out.println("Given your input, there is at least one real solution.");
 			
 			double solution1 = test.getSolution1();
 			double solution2 = test.getSolution2();
 			
+			System.out.println();
 			System.out.println("The solutions are " + solution1 + " and " + solution2 + ".");
 		
 		
 		
 		} else
-			System.out.println("There are no real solutions given your input.");
+			System.out.println("Given your input, there are no real solutions.");
 		
 		
 	input.close();	
